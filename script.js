f5d363297e4fe0650567c10cced0b89ac369f6c5
Gareth Field <field.gareth@gmail.com>
2021-04-10 09:44:51 -0400

    good morning!

--- script.js
+++ script.js
@@ -10,64 +10,154 @@ import {
 	spin,
 } from './utils.js';
 
-const projects = [
-	{
-		name: 'tech-resume',
-		weight: 0,
-	},
-	{
-		name: 'non-tech-resume',
-		weight: 0,
-	},
-	{
-		name: 'garethfield.com',
-		weight: 1,
-	},
-	{
-		name: 'master-blaster',
-		weight: 1,
-	},
-	{
-		name: 'HupPupApp',
-		weight: 2,
-	},
-	{
-		name: 'DawaTracker3M',
-		weight: 1,
-	},
-	{
-		name: 'polar-pairs',
-		weight: 1,
-	},
-	{
-		name: 'dot-eater',
-		weight: 1,
-	},
-	{
-		name: 'MultiBounce',
-		weight: 1,
-	},
-	{
-		name: 'houdini.how',
-		weight: 1,
-	},
-	{
-		name: 'Java MOOC',
-		weight: 0,
-	},
-];
+// ToDo: Two Main Goals:
+// ? 1) Create other lists of tech.
+// 		` put the stack in Stack Roulette `
+// ? 2) ~~*~~*~~ Abstraction! ~~*~~*~~
+// 		`	         ^^^^^^^^^^^^           `
+
+// * 1) ~ Lists! ~
+// * 			- Projects
+// * 			- JS Frameworks/Vanilla
+// * 			- Backend Solutions
+// * 			- Individual Technologies
+// * 			- Design Tools
+// * 			- Career-y Stuff
+
+// "projects"
+// const projects = [
+// 	{
+// 		name: 'SM64',
+// 		weight: 2,
+// 	},
+// 	{
+// 		name: 'Zelda OoT',
+// 		weight: 2,
+// 	},
+// 	{
+// 		name: 'Zelda MM',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'Agar.io',
+// 		weight: 1,
+// 	},
+// ];
+// const projects = [
+// 	{
+// 		name: 'gmod',
+// 		weight: 2,
+// 	},
+// 	{
+// 		name: 'chickydoo',
+// 		weight: 2,
+// 	},
+// 	{
+// 		name: 'playps',
+// 		weight: 2,
+// 	},
+// ];
+// const projects = [
+// 	{
+// 		name: 'Stack Roulette',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: '`dot-eater`',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'dawaTracker3M',
+// 		weight: 1
+// 	},
+// 	{
+// 		name: '`master-blaster`',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'garethfield.com',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'garethfield.web',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'HupPup',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'Polar Pairs',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'Pom',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'GarethTracker2000',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'Coding podcast!',
+// 		weight: 1,
+// 	},
+// ];
+// const projects = [
+// 	{
+// 		name: 'Super Mario 64',
+// 		weight: 3,
+// 	},
+// 	{
+// 		name: 'Legend of Zelda: OoT',
+// 		weight: 2,
+// 	},
+// 	{
+// 		name: 'Legend of Zelda: MM',
+// 		weight: 2,
+// 	},
+// 	{
+// 		name: 'Worms - Armegeddon',
+// 		weight: 1,
+// 	},
+// ];
+// const projects = [
+// 	{
+// 		name: 'pong',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'space invaders',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'super mario bros',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'asteroids',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'frogger',
+// 		weight: 1,
+// 	},
+// 	{
+// 		name: 'legend of zelda',
+// 		weight: 1,
+// 	},
+// ];
 
 const init = () => {
-	// Add colors and random position ˇˇˇ
+	// * Add colors and random position ˇˇˇ * //
 	const assignedProjects = getAssignedProjects(projects);
-	// Split out the projects object into individual roulette pieces ˇˇˇ
+	// * Split out the projects object into individual roulette pieces ˇˇˇ * //
 	const slices = getSlices(assignedProjects);
-	// Populate the DOM, may it flourish ˇˇˇ
+	// * Populate the DOM, may it flourish ˇˇˇ * //
 	slices.forEach(slice => {
 		const el = document.createElement('div');
 		el.id = slice.key;
 		el.className = 'slice';
-		console.log(getBackgroundImage({ assignedProjects, slice }));
 		el.style.border = `4px solid ${slice.borderColor}`;
 		el.style.zIndex = slice.key;
 		const projectName = document.createElement('span');
@@ -94,43 +184,14 @@ const init = () => {
 		}deg)`;
 		el.appendChild(projectName);
 		el.style.backgroundImage = getBackgroundImage({ assignedProjects, slice });
-		document.getElementById(
-			'roulette'
-		).style.background = dontPickBackgroundColor(htmlColors.random());
 		document.getElementById('roulette').appendChild(el);
-		document.getElementById('center').style.background =
-			slices[slices.length - 1].borderColor;
 	});
+	document.getElementById('roulette').style.background = slices[0].color;
+	document.getElementById('center').style.background =
+		slices[slices.length - 1].borderColor;
 	// Add event listener(s) ˇˇˇ
 	const button = document.getElementById('spinButton');
 	button.addEventListener('click', spin);
 };
 
 init();
-
-// const projects = [
-// 	{
-// 		name: 'gta',
-// 		weight: 2,
-// 	},
-// 	{
-// 		name: 'ttt',
-// 		weight: 2,
-// 	},
-// 	{
-// 		name: 'worms',
-// 		weight: 1,
-// 	},
-// 	{
-// 		name: 'chicky-d00',
-// 		weight: 2,
-// 	},
-// 	{
-// 		name: 'playps',
-// 		weight: 1,
-// 	},
-// 	{
-// 		name: 'rainbox-secvn',
-// 		weight: 1,
-// 	},
-// ];
