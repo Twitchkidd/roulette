81e78a69a07342c6b6ef06dd7550628f6ee8b36e
Gareth Field <field.gareth@gmail.com>
2022-06-23 08:44:26 -0400

    why do the data that way? shruggie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from './components/Container';
+import Header from './components/Header';
+import Roulette from './components/Roulette';
+import Footer from './components/Footer';
+import data from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+	<Container>
+		<Header>Schedule Roulette!</Header>
+		<Roulette data={data} />
+		<Footer />
+	</Container>
+);
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,101 @@
+const sbSoftware = 'Sun Bear Software';
+const huppup = 'HupPup';
+const hupdog = 'HupDog';
+const runclub = 'RunClub';
+const sweepers = 'The Sweepers';
+
+const projects = [
+	{
+		name: 'hupdog.com',
+		org: sbSoftware,
+	},
+	{
+		name: 'huppup.app',
+		org: sbSoftware,
+	},
+	{
+		name: 'saeedsmarket.com',
+		org: sbSoftware,
+	},
+	{
+		name: 'Looking For Legs',
+		org: huppup,
+	},
+	{
+		name: 'HupPup biz card',
+		org: huppup,
+	},
+	{
+		name: 'HupPup rules',
+		org: huppup,
+	},
+	{
+		name: 'HupDog method, low commentary',
+		org: hupdog,
+	},
+	{
+		name: 'Video-augmented blog post',
+		org: hupdog,
+	},
+	{
+		name: 'Highly commentated full video',
+		org: hupdog,
+	},
+	{
+		name: 'HupDog biz card',
+		org: hupdog,
+	},
+	{
+		name: 'RunClub Flier',
+		org: runclub,
+	},
+	{
+		name: 'RunClub rules and operations',
+		org: runclub,
+	},
+	{
+		name: "RunClub 'biz' card",
+		org: runclub,
+	},
+	{
+		name: 'Sweepers flier',
+		org: sweepers,
+	},
+	{
+		name: 'Sweepers mission',
+		org: sweepers,
+	},
+	{
+		name: 'Sweepers goal',
+		org: sweepers,
+	},
+	{
+		name: "Sweepers 'biz' card",
+		org: sweepers,
+	},
+];
+
+const days = [
+	[6, 23],
+	[6, 24],
+	[6, 25],
+	[6, 26],
+	[6, 27],
+	[6, 28],
+	[6, 29],
+	[6, 30],
+	[7, 1],
+	[7, 2],
+	[7, 3],
+	[7, 4],
+	[7, 5],
+	[7, 6],
+	[7, 7],
+	[7, 8],
+	[7, 9],
+];
+
+export const data = {
+	projects,
+	days,
+};
